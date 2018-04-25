64bbfd635605f6ed8327738394adfe8d1b34572f
Anders Emil Salvesen <andersemil.salvesen@skatteetaten.no>
2018-04-25 08:58:09 +0200

    Uppercase menu items

--- src/components/SegmentSelect.js
+++ src/components/SegmentSelect.js
@@ -29,7 +29,7 @@ const SegmentSelect = ({history, currentSegment}) => {
             >
                 <MenuItem className="segment-select-menu-item" id="overall">Overall</MenuItem>
                 {segments.map(segment => {
-                    return <MenuItem id={segment.id} key={segment.id}>{segment.name}</MenuItem>
+                    return <MenuItem className="segment-select-menu-item" id={segment.id} key={segment.id}>{segment.name}</MenuItem>
                 })}
             </SimpleMenu>
         </div>
